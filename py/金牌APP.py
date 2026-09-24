e8924f8ff7efbb69d7500972cb0bb8725ddb8e8e
80947108 <42614839+80947108@users.noreply.github.com>
2026-09-24 08:33:20 +0800

    Update home_url to new website

--- py/金牌APP.py
+++ py/金牌APP.py
@@ -15,7 +15,7 @@ class Spider(Spider):
         return "Aidianying"
 
     def init(self, extend):
-        self.home_url = 'https://m.sdzhgt.com/'
+        self.home_url = 'https://www.lwdys.com'
         self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
         self.error_url = "https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4"
 
